d893053704bc557bda1ae7d0b573f7debe94c50e
Kunaal <kunaal@users.noreply.github.com>
2020-05-26 18:55:57 +0530

    Update Retail Case Study Business Questions Solution 24 May2020.sql

--- SQL Training/1 Session Retail Case Study/Retail Case Study Business Questions Solution 24 May2020.sql
+++ SQL Training/1 Session Retail Case Study/Retail Case Study Business Questions Solution 24 May2020.sql
@@ -7,6 +7,20 @@ SELECT
 FROM retail_data_transactions
 limit 5;
 
+#Year on Year
+SELECT 
+right(trans_date,2) + 2000 as Year
+, count(customer_id) as purchases
+FROM retail_data_transactions
+group by right(trans_date,2) + 2000
+order by 1 asc
+;
+
+SELECT trans_date,RIGHT(trans_date,2) + 2000 as Year
+FROM retail_data_transactions
+limit 5;
+
+
 
 
 
